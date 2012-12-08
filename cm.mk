e154f01cde76d9f510c2e4785a301a74f8d0801c
Stelios Panayi <stilianos1000@gmail.com>
2012-12-08 15:32:47 +0200

    Update for 4.2

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/jbmp/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/semc/robyn/device.mk)
@@ -10,17 +10,16 @@ $(call inherit-product, device/semc/robyn/device.mk)
 # Setup device configuration
 PRODUCT_RELEASE_NAME := E10i
 PRODUCT_DEVICE := robyn
-PRODUCT_NAME := jbmp_robyn
+PRODUCT_NAME := cm_robyn
 PRODUCT_BRAND := SEMC
 PRODUCT_MODEL := E10i
 PRODUCT_MANUFACTURER := Sony Ericsson
 
 # Release data
-PRODUCT_VERSION_DEVICE_SPECIFIC := 1b
+PRODUCT_VERSION_DEVICE_SPECIFIC := 01
 TARGET_NO_LIVEWALLPAPERS := false
 TARGET_BOOTANIMATION_NAME := vertical-240x320
-TARGET_INCLUDE_GAPPS := true
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E10 BUILD_FINGERPRINT="google/yakju/maguro:4.1.1/JRO03C/398337:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.1.1 JRO03C 398337 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=E10 BUILD_FINGERPRINT="google/takju/maguro:4.2/JOP40C/517167:user/release-keys" PRIVATE_BUILD_DESC="takju-user 4.2 JOP40C 517167 release-keys"
 
